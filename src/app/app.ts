ad17bd5d613beeeb9aed063164f19a3f4740cd09
Felipe Luz <207072918+fsluz@users.noreply.github.com>
2025-06-14 12:26:09 -0300

    home

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { HorizontalNavbar } from './horizontal-navbar/horizontal-navbar';
+import { Slider } from './slider/slider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, HorizontalNavbar],
+  imports: [RouterOutlet, Navbar, HorizontalNavbar, Slider],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
