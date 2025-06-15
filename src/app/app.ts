726bb597e9d638c46ebd732d10f804f937e9465c
Felipe Luz <207072918+fsluz@users.noreply.github.com>
2025-06-14 22:37:00 -0300

    a

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from './navbar/navbar';
 import { HorizontalNavbar } from './horizontal-navbar/horizontal-navbar';
 import { Slider } from './slider/slider';
 import { Musics } from './musics/musics';
+import { Player } from './player/player';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, HorizontalNavbar, Slider, Musics],
+  imports: [RouterOutlet, Navbar, HorizontalNavbar, Slider, Musics, Player],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
